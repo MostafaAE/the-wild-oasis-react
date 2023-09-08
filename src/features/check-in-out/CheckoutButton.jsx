31c17a0432d023cc16d8f191111431fe79d3e0ef
MostafaAE <mostafa.aymanae@gmail.com>
2023-09-08 19:57:12 +0300

    Implement today activity panel

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,17 @@
-import Button from "../../ui/Button";
+import Button from '../../ui/Button';
+import SpinnerMini from '../../ui/SpinnerMini';
+import useCheckout from '../bookings/useCheckout';
 
 function CheckoutButton({ bookingId }) {
+  const { checkOut, isCheckingOut } = useCheckout();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      onClick={() => checkOut(bookingId)}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
