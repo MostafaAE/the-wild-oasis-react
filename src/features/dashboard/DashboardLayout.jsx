6d878b49be243c28a18257e746b9acba42ab6040
MostafaAE <mostafa.aymanae@gmail.com>
2023-09-08 04:21:35 +0300

    Compute recent stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import useRecentBookings from './useRecentBookings';
+import Spinner from '../../ui/Spinner';
+import useRecentStays from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -10,6 +12,11 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { isLoading1, bookings } = useRecentBookings();
+  const { isLoading2, stays, confirmedStays } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
+  console.log(bookings);
   return (
     <StyledDashboardLayout>
       <div>1</div>
